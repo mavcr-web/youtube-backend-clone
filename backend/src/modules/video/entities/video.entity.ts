3e0584e0d29666b7893de4dfa0646b58ca11e9fd
mavcr <mianvacrro@gmail.com>
2024-03-04 16:02:15 +0000

    Add Video entity and VideoService constructor

--- backend/src/modules/video/entities/video.entity.ts
+++ backend/src/modules/video/entities/video.entity.ts
@@ -1,1 +1,22 @@
-export class Video {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Video {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: [] })
+  tags: string[];
+
+  @Column()
+  uploadDate: Date;
+
+  @Column()
+  idUser: number;
+}
--- backend/src/modules/video/video.service.ts
+++ backend/src/modules/video/video.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { Video } from './entities/video.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VideoService {
+  constructor(
+    @InjectRepository(Video)
+    private readonly videoRepository: Repository<Video>,
+  ) {}
+
   create(createVideoDto: CreateVideoDto) {
     return 'This action adds a new video';
   }
