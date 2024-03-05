abdd4a0f9db274f03ad6136738abb3f164011c20
mavcr <mianvacrro@gmail.com>
2024-03-05 04:24:00 +0000

    Add dotenv package and configure AWS S3 client

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import * as dotenv from "dotenv";
+
+
 async function bootstrap() {
+
+  dotenv.config();
+  
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
--- backend/src/modules/video/video.controller.ts
+++ backend/src/modules/video/video.controller.ts
@@ -16,16 +16,26 @@ import { UpdateVideoDto } from './dto/update-video.dto';
 import { ApiBody, ApiConsumes, ApiParam, ApiTags } from '@nestjs/swagger';
 import { diskStorage } from 'multer';
 
+import { PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import { createReadStream } from 'fs';
+
 @ApiTags('video')
 @Controller('video')
 export class VideoController {
+  private s3Client = new S3Client({
+    region: process.env.AWS_BUCKET_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
   constructor(private readonly videoService: VideoService) {}
 
   @Post()
   @UseInterceptors(
     FileInterceptor('video', {
       storage: diskStorage({
-        destination: './uploads',
+        destination: 'uploads',
         filename: (req, file, cb) => {
           const filename: string = file.originalname.split('.')[0];
           const fileExtName: string = file.originalname.split('.')[1];
@@ -47,10 +57,27 @@ export class VideoController {
     },
   })
   async create(@UploadedFile() file: Express.Multer.File) {
-    console.log('file', file);
-    
-    console.log(file);
+    console.log(process.env.AWS_BUCKET_NAME);
+    console.log(process.env.AWS_BUCKET_REGION);
+    console.log(process.env.AWS_ACCESS_KEY_ID);
+    console.log(process.env.AWS_SECRET_ACCESS_KEY);
+    console.log(this.s3Client);
 
+    console.log('file', file);
+
+    try {
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+      });
+
+      const response = await this.s3Client.send(command);
+      console.log('response', response);
+    } catch (error) {
+      console.log('error', error);
+      
+    }
     return file.filename;
   }
   // create(@Body() createVideoDto: CreateVideoDto, @UploadedFile() file: Express.Multer.File) {
