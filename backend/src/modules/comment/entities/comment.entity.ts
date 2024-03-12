931d9698af562369026bc30ba6b526fa055896f5
mavcr <mianvacrro@gmail.com>
2024-03-12 05:12:08 +0000

    new branch

--- backend/src/modules/comment/entities/comment.entity.ts
+++ backend/src/modules/comment/entities/comment.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Comment {
@@ -10,6 +11,9 @@ export class Comment {
 
   @Column()
   idUser: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
 
   @Column()
   username: string;
